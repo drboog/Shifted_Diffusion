1d9e863f852a2de3bc352f59944c1d738526e0fd
drboog <yufanzhou1993@gmail.com>
2023-05-18 12:53:37 -0400

    Update train.py

--- train.py
+++ train.py
@@ -350,6 +350,7 @@ def main():
     train_transforms = Compose(
         [ToTensor(),
          Resize(224, interpolation=BICUBIC),
+         CenterCrop(224),
          Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
          ]
     )
